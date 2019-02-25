ecde273a6f56d7086172616a8e656c71c353c9b8
chay <chay>
2019-02-25 17:29:17 +0800

    2019/2/25

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <router-view name='header'></router-view>
+    <router-view name='home'></router-view>
   </div>
 </template>
 
@@ -12,13 +12,5 @@ export default {
 </script>
 
 <style lang='scss'>
-$color:red;
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: $color;
-  margin-top: 60px;
-}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/page/home'
+import Header from '@/components/Header'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+  mode: 'history',
+  routes: [{
+    path: '/',
+    components: {
+      'home': Home,
+      'header': Header
     }
-  ]
+  }]
 })
